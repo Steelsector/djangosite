765b4593c6a300324bf26b9e84e476d0055702b5
Bernáth Viktor <Bernth Viktor>
2016-04-21 21:56:48 +0200

    template

--- static/js/base.js
+++ static/js/base.js
@@ -2,3 +2,40 @@
  * Created by steel on 2016. 04. 08..
  */
 //alert('asdsa');
+$(document).ready(function () {
+
+    var circlesCreated = false;
+
+    function onScroll() {
+        if (!circlesCreated && $('#skills').visible()) {
+            circlesCreated = true;
+            createCircles();
+        }
+    }
+
+    function createCircles() {
+        $('.precent').each(function (i, obj) {
+
+            var child = this.id;
+            percentage = this.getAttribute('value') / 100;
+            $("#" + child).circleProgress({
+                value: percentage,
+                size: 80,
+                fill: {
+                    gradient: ["navy", "turquoise"]
+                }
+            }).on('circle-animation-progress', function (event, progress) {
+                $(this).find('strong').html(parseInt(this.getAttribute('value') * progress) + '<i>%</i>'
+                )
+                ;
+            });
+        });
+    }
+
+    if ($('#skills').visible() != true) {
+        window.onscroll = onScroll;
+    } else {
+        createCircles();
+    }
+
+});
